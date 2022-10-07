62a52ed899e257eaa2ae5f8ab283d8a02ebd528c
Rohan Gupta <36019794+RGupta323@users.noreply.github.com>
2022-10-07 08:44:12 -0700

    Update DevDeploy
    Created a test stack and modified DevDeploy to run commands only on the dev/test stack

--- bin/fitness_app.js
+++ bin/fitness_app.js
@@ -4,7 +4,7 @@ const cdk = require('aws-cdk-lib');
 const { FitnessAppStack } = require('../lib/infra/fitness_app-stack');
 
 const app = new cdk.App();
-new FitnessAppStack(app, 'FitnessAppStack', {
+new FitnessAppStack(app, 'FitnessApp-Test-Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
