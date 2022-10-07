d0aecdfcfa1668e659a38d4faec020ce513c22b0
Rohan Gupta <guptarohan323@gmail.com>
2022-10-06 20:09:40 -0700

    Initial commit

---
+++ test/fitness_app.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const FitnessApp = require('../lib/fitness_app-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fitness_app-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new FitnessApp.FitnessAppStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
