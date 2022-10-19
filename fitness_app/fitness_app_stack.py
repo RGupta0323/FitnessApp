11f5c633d6d36685bdd06e81ed5a4f442eb0406c
Rohan Gupta <36019794+RGupta323@users.noreply.github.com>
2022-10-19 11:38:39 -0700

    fixing bugs in infra stack

--- fitness_app/fitness_app_stack.py
+++ fitness_app/fitness_app_stack.py
@@ -6,6 +6,7 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as _lambda
 )
 
 
@@ -14,4 +15,7 @@ class FitnessAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-
+        home_lambda = _lambda.Function(self, id="HomeLambda",
+                                   runtime=_lambda.Runtime.PYTHON_3_7,
+                                   code=_lambda.Code.from_asset("src"),
+                                   handler="homepage_lambda.handler")
