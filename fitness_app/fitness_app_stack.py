8ee8637bd7693c94f89ec7d6b37fa2eda3bf44f0
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2022-12-14 08:00:46 -0600

    Adding api gateway

--- fitness_app/fitness_app_stack.py
+++ fitness_app/fitness_app_stack.py
@@ -7,7 +7,7 @@ from aws_cdk import (
     aws_sns as sns,
     aws_sns_subscriptions as subs,
     aws_lambda as _lambda,
-    aws_apigatewayv2 as apigateway
+    aws_apigateway as apigateway
 )
 
 
@@ -20,8 +20,4 @@ class FitnessAppStack(Stack):
                                    runtime=_lambda.Runtime.PYTHON_3_7,
                                    code=_lambda.Code.from_asset("src"),
                                    handler="homepage_lambda.handler")
-
-        api = apigateway.LambdaRestApi(self, "Figit tnessApp-API-Gateway",
-                                       handler=home_lambda,
-                                       proxy=False
-                                       )
+        api = apigateway.LambdaRestApi()
