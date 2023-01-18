4b7662c8361da8ee2c50e0c681972f62c9eeb14f
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-01-18 08:35:40 -0600

    configuring register.html to get webpage from s3

--- src/register_lambda.py
+++ src/register_lambda.py
@@ -1,9 +1,10 @@
 import os
 import sys
 from jinja2 import Environment, FileSystemLoader
+from lambda_utils import get_contents_s3_obj
 def handler(event, context):
     print("homepage_lambda handler called")
-    env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "web"), encoding="utf8"))
+    '''env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "web"), encoding="utf8"))
     template = env.get_template("register.html")
     html = template.render()
     return {
@@ -13,4 +14,14 @@ def handler(event, context):
                 "Content-Type":"text/html"
             },
             'body': html
+    }'''
+    html = get_contents_s3_obj(bucket_name="fitness-app-dev-stack-fitnessappstaticwebfiles659-1c9bv2im68wv0",
+                               object_key="register.html")
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "text/html"
+        },
+        'body': html.decode()
     }
