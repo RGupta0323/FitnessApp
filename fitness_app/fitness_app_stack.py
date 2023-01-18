0b52e7a38fec826efdb9108a586a3b5ebd7f6286
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-01-18 09:56:08 -0600

    fixing bugs

--- fitness_app/fitness_app_stack.py
+++ fitness_app/fitness_app_stack.py
@@ -40,7 +40,8 @@ class FitnessAppStack(Stack):
         register_endpoint = api.root.add_resource("register") # adding endpiont in api gateway for register
         register_endpoint.add_method("GET", apigateway.LambdaIntegration(register_lambda))
 
-        register_submit_form_endpoint = api.root.add_resource("")
+        register_submit_form_endpoint = api.root.add_resource("register_form_submit")
+        register_submit_form_endpoint.add_method("GET", apigateway.LambdaIntegration(register_submit_form_lambda))
 
         # creating s3 bucket for static web pages
         web_files_bucket = s3.Bucket(self, "FitnessAppStaticWebFiles", encryption=s3.BucketEncryption.KMS_MANAGED,
