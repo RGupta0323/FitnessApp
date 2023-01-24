8f3f1515041c81b49b309d2b8833bb1263843f97
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-01-24 12:44:11 -0600

    create dynamodb table for user data

--- src/register_submit_form_lambda.py
+++ src/register_submit_form_lambda.py
@@ -6,6 +6,13 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('FitnessAppUserData')
 def handler(event, context):
+    # query dynamo db to see if the user is in the database
+
+    # if the user is in the database then display a message indicating that a user with that email has an account and
+    # re-direct to login page.
+
+    # if the user is not in the database, then proceed
+    # If its the same email - then the lambda should re-direct the user to
     table.put_item(Item=event)
 
     # redirect user to the login page & log user in
