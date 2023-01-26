9a9a9b041eefba483dcd3d58dfbdec0eaae28e6b
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-01-25 22:24:38 -0600

    modifying lambda

--- src/register_submit_form_lambda.py
+++ src/register_submit_form_lambda.py
@@ -8,6 +8,7 @@ def handler(event, context):
     print("register_submit_form_lambda triggered")
     print("Event: {}".format(event))
     # query dynamo db to see if the user is in the database
+    """
     query_response= None
     try:
         client = boto3.client('dynamodb', region_name="us-east-1")
@@ -22,9 +23,14 @@ def handler(event, context):
 
     # if the user is not in the database, then proceed
     # If its the same email - then the lambda should re-direct the user to
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('FitnessAppUserData')
-    # table.put_item(Item=event)
+    """
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('fitness-app-dev-stack-FitnessAppUserData5D9F0F31-YQPUN4XKQ00I')
+        table.put_item(Item=event)
+
+    except Exception as ex:
+        return {"statuscode": 400, "message":"Error occured while inserting data in dynamodb. Exception: {}".format(ex)}
 
     # redirect user to the login page & log user in
     return {"statuscode": 200 }
