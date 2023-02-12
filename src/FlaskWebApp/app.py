999aa00a3daa421245b0116f513546bcfa19feea
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-02-12 11:02:41 -0600

    configuring register endpoint

--- src/FlaskWebApp/app.py
+++ src/FlaskWebApp/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import serverless_wsgi
 app = Flask(__name__)
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        print("line 11 - inside /register")
+        fname = request.form.get("fname")
+        print(fname)
+        # getting input with name = lname in HTML form
+        lname = request.form.get("lname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        event={"fname": fname, "lname": lname, "email":email, "password":password}
+        print("event: {}".format(event))
+
+        # add user data to dynamo - use congito for user verification!??
+    return render_template("register.html")
 
 def handler(event, context):
     return serverless_wsgi.handle_request(app, event, context)
