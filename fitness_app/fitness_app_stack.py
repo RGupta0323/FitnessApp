57715516b7c1eab655323fd747eb69585237fd35
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-02-12 22:06:08 -0600

    adding cognito for user auth

--- fitness_app/fitness_app_stack.py
+++ fitness_app/fitness_app_stack.py
@@ -5,13 +5,12 @@ from aws_cdk import (
     Duration,
     Stack,
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
     aws_s3 as s3,
-    aws_dynamodb as dynamodb
+    aws_dynamodb as dynamodb,
+    aws_cognito as cognito,
+    RemovalPolicy
 )
 
 
@@ -29,6 +28,58 @@ class FitnessAppStack(Stack):
 
 
 
+        # cognito - for user auth
+        cognito_user_pool = cognito.UserPool(self, "awesome-user-pool",
+                                             user_pool_name="awesome-user-pool",
+                                             sign_in_aliases=cognito.SignInAliases(
+                                                 email=True
+                                             ),
+                                             self_sign_up_enabled=True,
+                                             auto_verify=cognito.AutoVerifiedAttrs(
+                                                 email=True
+                                                 # This is True by default if email is defined in SignInAliases
+                                             ),
+                                             user_verification=cognito.UserVerificationConfig(
+                                                 email_subject="You need to verify your email",
+                                                 email_body="Thanks for signing up Your verification code is {####}",
+                                                 # This placeholder is a must if code is selected as preferred verification method
+                                                 email_style=cognito.VerificationEmailStyle.CODE
+                                             ),
+                                             standard_attributes=cognito.StandardAttributes(
+                                                 family_name=cognito.StandardAttribute(
+                                                     required=True,
+                                                     mutable=False,
+                                                 ),
+                                                 address=cognito.StandardAttribute(
+                                                     required=False,
+                                                     mutable=True
+                                                 )
+                                             ),
+                                             custom_attributes={
+                                                 "tenant_id": cognito.StringAttribute(min_len=10, max_len=15,
+                                                                                      mutable=False),
+                                                 "created_at": cognito.DateTimeAttribute(),
+                                                 "employee_id": cognito.NumberAttribute(min=1, max=100, mutable=False),
+                                                 "is_admin": cognito.BooleanAttribute(mutable=True),
+                                             },
+                                             password_policy=cognito.PasswordPolicy(
+                                                 min_length=8,
+                                                 require_lowercase=True,
+                                                 require_uppercase=True,
+                                                 require_digits=True,
+                                                 require_symbols=True
+                                             ),
+                                             account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
+                                             removal_policy=RemovalPolicy.DESTROY
+                                             )
+
+        app_client = cognito_user_pool.add_client(
+            "awesome-app-client",
+            user_pool_client_name="awesome-app-client",
+            auth_flows=cognito.AuthFlow(
+                user_password=True
+            )
+        )
 
 
         # creating s3 bucket for static web pages
