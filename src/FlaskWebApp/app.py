6052f1dfa8b63b1b92323996d603277bc4fa1c58
RGupta0323 <36019794+RGupta323@users.noreply.github.com>
2023-03-04 02:14:28 -0600

    gotten teh /createexercise endpoint to add exercises to a table in createworkout.html in proper format

--- src/FlaskWebApp/app.py
+++ src/FlaskWebApp/app.py
@@ -12,19 +12,39 @@ def index():
 def home():
     return render_template("Home.html")
 
+
 @app.route("/createworkout", methods=["GET", "POST"])
 def createworkout():
     # This if statement will be hit if someone has created an exercise and that should be shown on the createworkout page
     # so the user can create more exercises
     if(request.method == 'POST'):
         req_form = request.form
-        keys = req_form.getlist(0)
-        values = req_form.getlist(1)
-        d = dict()
-        for i in range(len(keys)):
-            d[keys[i]] = values[i]
+        print(req_form)
+        keys = req_form.keys()
+        values = req_form.values()
+        print("keys: {}".format(keys))
+        print("values: {}".format(values))
 
-        print("Dictionary created: {}".format(d))
+        # this is a didctionary based on the users response
+        # it will look like this: {'set1weight': '200', 'set1reps': '3', 'set2weight':"", 'set3reps':''}
+        user_dict = {element:req_form.get(element) for element in req_form}
+        print("[/creatworkout endpoint app.py line 30] user_Dict: {}".format(user_dict))
+
+        # Transform dictionary to look like this {"1": {'weight': '200", 'reps': "4"} "2":{'weight':'200', 'reps':'5'}}
+        print(" [/createworkout endpoint] transforming user_Dict ")
+        num_sets = int(max([letter for element in user_dict.keys() for letter in element if (letter.isdigit())]))
+        modified_dict = {i + 1: {'weight': user_dict["set{}weight".format(i + 1)], 'reps': user_dict["set{}reps".format(i + 1)]} for i
+                         in range(num_sets)}
+        modified_dict["exercise"] = user_dict["exercise"]
+
+        print("modified_dict: {}".format(modified_dict))
+
+
+        # Now this dictionary will go back to createworkout.html, displaying the created exercise.
+        return render_template("createworkout.html", user_dict = modified_dict)
+
+
+
     return render_template("createworkout.html")
 
 @app.route("/createexercise", methods=["GET", "POST"])
